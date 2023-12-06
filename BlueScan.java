a4d25fa5b29b337228c8b0f4f06beae0b5c2548e
ELIJAHH <144183598+eliiiii-lam@users.noreply.github.com>
2023-12-05 17:50:46 -0800

    Add files via upload

---
+++ BlueScan.java
@@ -0,0 +1,199 @@
+/*
+ * Copyright (c) 2019 OpenFTC Team
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+
+@Autonomous (name = "BlueScan", group = "Barcode")
+public class BlueScan extends LinearOpMode {
+
+    // Define motors and servos
+
+    /*DcMotor fL;
+    DcMotor fR;
+    DcMotor bL;
+    DcMotor bR;
+*/
+    OpenCvCamera webcam;
+
+    BarcodeBlue pipeline = new BarcodeBlue(telemetry);
+
+    @Override
+    public void runOpMode() {
+        //initialize robot hardware
+
+       /* fL = hardwareMap.dcMotor.get("fL");
+        fR = hardwareMap.dcMotor.get("fR");
+        bL = hardwareMap.dcMotor.get("bL");
+        bR = hardwareMap.dcMotor.get("bR");
+*/
+
+        //FOR THE WEBCAM
+        /*
+         * Instantiate an OpenCvCamera object for the camera we'll be using.
+         * Webcam stream goes to RC phone
+         */
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+
+        //webcam.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
+
+        //webcam.setPipeline(pipeline);
+//waitForStart();
+        /*
+         * Open the connection to the camera device. New in v1.4.0 is the ability
+         * to open the camera asynchronously which allows faster init time, and
+         * better behavior when pressing stop during init (i.e. less of a chance
+         * of tripping the stuck watchdog)
+         */
+        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                /*
+                 * Tell the webcam to start streaming images to us! Note that you must make sure
+                 * the resolution you specify is supported by the camera. If it is not, an exception
+                 * will be thrown.
+                 *
+                 * Keep in mind that the SDK's UVC driver (what OpenCvWebcam uses under the hood) only
+                 * supports streaming from the webcam in the uncompressed YUV image format. This means
+                 * that the maximum resolution you can stream at and still get up to 30FPS is 480p (640x480).
+                 * Streaming at e.g. 720p will limit you to up to 10FPS and so on and so forth.
+                 *
+                 * Also, we specify the rotation that the webcam is used in. This is so that the image
+                 * from the camera sensor can be rotated such that it is always displayed with the image upright.
+                 * For a front facing camera, rotation is defined assuming the user is looking at the screen.
+                 * For a rear facing camera or a webcam, rotation is defined assuming the camera is facing
+                 * away from the user.
+                 */
+                //320px x 340px
+                webcam.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
+
+                /*
+                 * Specify the image processing pipeline we wish to invoke upon receipt
+                 * of a frame from the camera. Note that switching pipelines on-the-fly
+                 * (while a streaming session is in flight) *IS* supported.
+                 */
+
+                webcam.setPipeline(pipeline);
+            }
+
+            @Override
+            public void onError(int errorCode) {
+                telemetry.addData("errorCode", errorCode);
+            }
+        });
+        // Tell telemetry to update faster than the default 250ms period :)
+        telemetry.setMsTransmissionInterval(20);
+
+        telemetry.addLine("Waiting for start");
+        telemetry.update();
+
+        //Wait for the user to press start on the Driver Station
+        waitForStart();
+
+        //Manages Telemetry and stopping the stream
+        while (opModeIsActive()) {
+
+            sleep(2000);
+            telemetry.addData("Analysis", pipeline.getAnalysis());
+            telemetry.update();
+
+            switch (pipeline.getAnalysis()) {
+                case LEFT:
+                    //turn correction
+                    //turnRight();
+                    //sleep(100);
+
+                    break;
+                case CENTER:
+                    //
+                    break;
+                case RIGHT:
+
+
+                    break;
+            }
+
+            //reminder to use the KNO3 auto transitioner once this code is working
+            webcam.stopStreaming();
+            webcam.closeCameraDevice();
+            break;
+        }
+    }
+}
+
+
+    /*public void forward() {
+        fL.setPower(.5);
+        fR.setPower(-.5);
+        bL.setPower(.5);
+        bR.setPower(-.5);
+    }
+
+    public void backwards() {
+        fL.setPower(-.3);
+        fR.setPower(.3);
+        bL.setPower(-.3);
+        bR.setPower(.3);
+    }
+
+    public void turnRight() {
+        fL.setPower(0);
+        fR.setPower(-.5);
+        bL.setPower(0);
+         bR.setPower(-.5);
+    }
+
+    public void turnLeft() {
+        fL.setPower(.5);
+        fR.setPower(0);
+        bL.setPower(0);
+        bR.setPower(.5);
+    }
+
+    public void stopMotors() {
+        fL.setPower(0);
+        fR.setPower(0);
+        bL.setPower(0);
+        bR.setPower(0);
+    }
+
+    public void Straferight() {
+        fL.setPower(.3);
+        bL.setPower(-.3);
+        fR.setPower(.3);
+        bR.setPower(-.3);
+    }
+    public void Strafeleft() {
+        fL.setPower(-.3);
+        bL.setPower(.3);
+        fR.setPower(-.3);
+        bR.setPower(0.3);
+    }
+}
+*/
